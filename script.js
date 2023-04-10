32a65d34cf30b72e75ad02231d138ffff697dc27
Richmondsterd <123800593+Richmondsterd@users.noreply.github.com>
2023-04-10 14:50:49 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript
+let counter = 3;
+
+function upvote() {
+  counter = counter + 1;
+
+  
+document.getElementById("votes")
+.innerHTML = counter + "votes";
+}
+
+function downvote() {
+  counter = counter - 1;
+  
+  
+  document.getElementById("votes")
+  .innerHTML = counter + "votes";
+}
